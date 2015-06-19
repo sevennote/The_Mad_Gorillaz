6e5f6330c78a7ab83da0d9fa815de4a1de0ea5c0
[sevennote] <[Joseph.Fields22@gmail.com]>
2015-06-19 19:39:58 -0400

    fixed direction paths

--- mad-gorillaz/app/controllers/sessions_controller.rb
+++ mad-gorillaz/app/controllers/sessions_controller.rb
@@ -5,12 +5,13 @@ class SessionsController < ApplicationController
 
   def create
   	user = User.find_by(username: user_params[:username])
-    byebug
   	if user && user.authenticate(user_params[:password])
   		session[:user_id] = user.id
   		redirect_to root_path
-  	end
+    else
+      
     redirect_to login_path
+    end
   end
 
   def destroy
