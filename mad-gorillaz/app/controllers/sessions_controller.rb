1e802fbd71cc268ecce80bbce27dde24402212ee
[sevennote] <[Joseph.Fields22@gmail.com]>
2015-06-19 19:37:02 -0400

    tested login

--- mad-gorillaz/app/controllers/sessions_controller.rb
+++ mad-gorillaz/app/controllers/sessions_controller.rb
@@ -5,16 +5,19 @@ class SessionsController < ApplicationController
 
   def create
   	user = User.find_by(username: user_params[:username])
-  	if user.authenticate(params[:user][:password])
+    byebug
+  	if user && user.authenticate(user_params[:password])
   		session[:user_id] = user.id
-  		redirect_to root
+  		redirect_to root_path
   	end
+    redirect_to login_path
   end
 
   def destroy
     sessions[:user_id] = nil
+    redirect_to root_path
   end
-
+ 
 
   private
 
