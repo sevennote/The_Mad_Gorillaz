2c567ddc75b837f2b1818365d3ec66f728e07b1b
[sevennote] <[Joseph.Fields22@gmail.com]>
2015-06-19 18:58:52 -0400

    added logic to login and logout

--- mad-gorillaz/app/controllers/sessions_controller.rb
+++ mad-gorillaz/app/controllers/sessions_controller.rb
@@ -4,11 +4,23 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	user = User.find_by(username: user_params[:username])
+  	if user.authenticate(params[:user][:password])
+  		session[:user_id] = user.id
+  		redirect_to root
+  	end
   end
 
   def destroy
+    sessions[:user_id] = nil
   end
 
+
+  private
+
+	def user_params
+	  params.require(:user).permit(:username, :password)
+	end
 end
 
 
