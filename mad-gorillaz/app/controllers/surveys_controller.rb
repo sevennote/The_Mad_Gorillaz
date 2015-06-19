f4bb2de91dc8a76d625827986ab4939bf9ba173b
tgoldenberg <thomasagoldenberg@gmail.com>
2015-06-19 17:00:25 -0400

    create controllers and html pages

---
+++ mad-gorillaz/app/controllers/surveys_controller.rb
@@ -0,0 +1,22 @@
+class SurveysController < ApplicationController
+  def create
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def owner_display
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
