fd93f8ef3cacc9b524f3a2ae7aa1ba04b5738f8c
Lowell Mower <lowell.m.mower@gmail.com>
2015-06-20 13:19:11 -0400

    add user creation and route path for signup

--- mad-gorillaz/app/controllers/users_controller.rb
+++ mad-gorillaz/app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
-  def create
+  def new
+    @user = User.new
   end
 
-  def new
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:notice] = "That username has already been used"
+      redirect_to new_user_path
+    end
   end
 
   def edit
@@ -16,4 +25,11 @@ class UsersController < ApplicationController
 
   def update
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
