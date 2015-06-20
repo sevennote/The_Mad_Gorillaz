9107d290641f1a85cc57d2735fb4350dd0ebe78f
tgoldenberg <thomasagoldenberg@gmail.com>
2015-06-20 17:06:32 -0400

    add React components for FormBox, RenderedQuestions, QuestionForm, QuestionField, and MultipleChoices

--- mad-gorillaz/app/assets/javascripts/surveys.js
+++ mad-gorillaz/app/assets/javascripts/surveys.js
@@ -1,9 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(function() {
-  $('.glyphicon-plus').on('click', function(event) {
-    event.preventDefault();
-    console.log($(event.target));
-    $('.hidden-fields').toggleClass('hidden');
-  })
-});
+// $(function() {
+//   $('.glyphicon-plus').on('click', function(event) {
+//     event.preventDefault();
+//     console.log($(event.target));
+//     $('.hidden-fields').toggleClass('hidden');
+//   })
+// });
